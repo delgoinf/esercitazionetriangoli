6e77b53f2784ba9842b3824544493513ec812232
andreadgobbo@gmail.com <andreadgobbo@gmail.com>
2020-11-29 13:18:24 +0100

    Aggiungere i file di progetto.

---
+++ libreriatriangoli/metodi.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace libreriatriangoli
+{
+    public class metodi
+    {
+        public static double Lato()
+        {
+            Console.WriteLine("inserire la lunghezza del lato ");
+            double l = double.Parse(Console.ReadLine());
+            return l;
+        }
+        public static string ClassificaTriangolo(double l1, double l2, double l3)
+        {
+            string risultato = "";
+            if (l1 > 0 && l2 > 0 && l3 > 0)
+            {
+                if (l1 != l2 && l1 != l3 && l2 != l3)
+                {
+                    Console.WriteLine("SCALENO");
+                }
+                else if (l1 == l2 && l2 == l3)
+                {
+                    Console.WriteLine("EQUILATERO");
+                }
+                else
+                Console.WriteLine("ISOSCELE");
+            }
+            else
+                Console.WriteLine("VALORI ERRATI");
+            return risultato;
+        }
+
+        public static double CalcolaPerimetro(double n1, double n2, double n3)
+        {
+            return n1 + n2 + n3;
+        }
+    }
+}
+
---
+++ metoditriangoli/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using libreriatriangoli;
+
+namespace metoditriangoli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double l1 = metodi.Lato();
+            double l2 = metodi.Lato();
+            double l3 = metodi.Lato();
+            double perimetro = metodi.CalcolaPerimetro(l1, l2, l3);
+            string risultato = metodi.ClassificaTriangolo(l1, l2, l3);
+            if (l1 < 0 || l2 < 0 || l3 < 0)
+            {
+                Console.WriteLine("ERRORE");
+            }
+            else
+            {
+                Console.WriteLine($"PERIMETRO={perimetro}");
+            }
+
+        }
+    }
+}
